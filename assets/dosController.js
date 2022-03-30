c6686a75cb155d24d3d6a5f2625a09853d1ad929
Carloslecval <carloslecval03@gmail.com>
2022-03-30 16:19:09 +0200

    completo

--- assets/dosController.js
+++ assets/dosController.js
@@ -36,3 +36,30 @@ function dosload() {
     canvas.height = imagen.height;
     canvas.width = imagen.width;
 }
+
+function getResult()
+{
+    document.getElementById("res").src = "";
+    let imageName = getCookie("imageName");
+    r = document.getElementById("gameCanvasR").toDataURL();
+    g = document.getElementById("gameCanvasB").toDataURL();
+    b = document.getElementById("gameCanvas").toDataURL();
+    const formData = new FormData();
+    formData.append("imageName", imageName);
+    formData.append("canvasR", r);
+    formData.append("canvasG", g);
+    formData.append("canvasB", b);
+    fetch(`http://localhost:5000/ifft`, {
+        method: 'POST',
+        mode: "cors",
+        body: formData
+    })
+    .then(response => {
+        response.json()
+    })
+    .then(data => {
+        document.getElementById("parrafoOutput").hidden = false;
+        document.getElementById("res").hidden = false;
+        document.getElementById("res").src = "https://fourierbucket.s3.us-west-2.amazonaws.com/" + imageName + "_transformed.png";
+    })
+}
